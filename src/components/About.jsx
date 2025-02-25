11b5cec078e4f44c22e4341302004ea8094a109e
dylanbforde <forde.dylan@gmail.com>
2025-02-25 10:30:17 +0000

    Assistant checkpoint: Added GitHub link to About section
    Assistant generated file changes:
    - src/components/About.jsx: Add GitHub information
    
    ---
    
    User prompt:
    
    This works now, do you have any suggestions on what can be done, I also want to add my github in the about me section, my username is dylanbforde

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export const ReadmeFile = () => {
   return (
     <div>
-      Welcome to my interactive CV! Navigate through directories about myself.
-      Use common commands. Type "help" for more information.
+      <p>Welcome to my interactive CV! Navigate through directories about myself.
+      Use common commands. Type "help" for more information.</p>
+      <p>Find me on GitHub: <a href="https://github.com/dylanbforde" style={{color: '#FFC200'}}>@dylanbforde</a></p>
     </div>
   );
 };
